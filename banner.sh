2e4111c56f1380a5ce25234f439f8731b9fb9147
linuxworkshop67 <78328905+linuxworkshop67@users.noreply.github.com>
2024-11-29 13:03:24 -0500

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,9 @@
+echo -e "┌───────────────────────────────────────┐"
+echo -e "│ Basic information about your system   │"
+echo -e "└───────────────────────────────────────┘"
+# Individual Characters:
+#  * This is the line: ─
+#  * These are the cornners:
+# ┌ ┐
+# └ ┘
+# │ 
